ca4fc86024f1a4d9d651b421b87e22e3bbfab1d4
slamice <slamice@slamices-MacBook-Pro.local>
2014-11-25 16:08:13 +0900

    moved all files to root for heroku setup

---
+++ db_repository/versions/004_migration.py
@@ -0,0 +1,33 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+user = Table('user', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('first_name', String(length=64)),
+    Column('last_name', String(length=64)),
+    Column('email', String(length=120)),
+    Column('ctime', DateTime),
+    Column('about_user', String(length=140)),
+    Column('mtime', DateTime),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['about_user'].create()
+    post_meta.tables['user'].columns['mtime'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    post_meta.tables['user'].columns['about_user'].drop()
+    post_meta.tables['user'].columns['mtime'].drop()
